a9cd08a62269baf98f9af94de90dbffba3e80004
xo4yxypmy <valik.kovalyk99@gmail.com>
2021-05-17 21:38:33 +0300

    first

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict"
+
+let name = 'John';
+let admin = name;
+
+console.log(admin);
+console.log('\n');
+
+console.log("Who's there?")
+let login = 'Myxa_B_CMETAHE';
+let password = 123456;
+
+if (login != 'Myxa_B_CMETAHE') {
+    console.log("I don't know you");
+} else if (login = 'Myxa_B_CMETAHE', password != 123456) {
+    console.log(login + ' What is your password?')
+    console.log('Wrong password')
+} else if (login = 'Myxa_B_CMETAHE', password = 123456) {
+    console.log(login + ' What is your password?')
+    console.log('Welcome!')
+} else {
+    console.log('Canceled')
+}
